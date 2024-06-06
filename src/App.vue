c508ff074736446449d3239433aa7bdeb4d3f72d
DipikaBhandari <bdipika076@gmail.com>
2024-06-06 16:20:38 +0200

    minor changes for checking and storing user role

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,10 @@
 <script setup>
-// Import necessary modules
 import { RouterView } from 'vue-router';
 import employeeNavbar from "@/components/EmployeeNavbar.vue";
 import customerNavbar from "@/components/Navbar.vue";
+import { useUserRole } from '@/stores/userRole'; // Update the import path if necessary
 
-import { useUserSessionStore } from '@/stores/UserSession'; // Import useUserSessionStore from your store module
-import { computed } from 'vue'; // Import computed from Vue
-
-// Access the userSessionStore from the Vuex store
-const userSessionStore = useUserSessionStore();
-
-// Check if the user is logged in
-const isLoggedIn = computed(() => {
-  return !!userSessionStore.user;
-});
-
-// If the user is logged in, get the user role from the store
-const userRole = computed(() => {
-  return isLoggedIn.value ? userSessionStore.user.role : null;
-});
-
-// Compute whether to show the Navbar based on the user's role
-const forEmployee = computed(() => {
-  return userRole.value === 'ROLE_EMPLOYEE';
-});
-const forCustomer = computed(() => {
-  return userRole.value === 'ROLE_CUSTOMER';
-});
-
+const { isLoggedIn, forEmployee, forCustomer } = useUserRole();
 </script>
 
 <template>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,33 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 import { Quasar, Notify } from 'quasar';
 import 'quasar/src/css/index.sass';
+import { useUserSessionStore } from '@/stores/UserSession';
 
+// Create the Vue application instance
+const app = createApp(App);
 
+// Create the Pinia instance
+const pinia = createPinia();
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
+// Use Pinia, Router, and Quasar plugins
+app.use(pinia);
+app.use(router);
 app.use(Quasar, {
     plugins: {
-        Notify
-    }
+        Notify,
+    },
 });
 
-app.mount('#app')
+// Initialize user session from token before mounting the app
+const userSessionStore = useUserSessionStore(pinia);
+userSessionStore.checkUserRole(); // Initialize user data from token on app initialization
+
+// Mount the application
+app.mount('#app');
