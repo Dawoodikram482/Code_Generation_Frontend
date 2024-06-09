43e0a2c566e6f8474fe099e663bdfb9da03dbac0
UgurS <42215100+UgurS@users.noreply.github.com>
2024-06-10 00:41:57 +0200

    Configured for deployment

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://meowbankapi.onrender.com'
 });
 
 // Add an interceptor to include authentication tokens
